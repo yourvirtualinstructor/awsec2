82379f130a8fba8e542447915c3b2944558ad063
Your Virtual Instructor <145182304+yourvirtualinstructor@users.noreply.github.com>
2023-09-18 17:53:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "us-east-2"
+  version = "~> 5.0"
+  region  = "us-east-1"
 }
