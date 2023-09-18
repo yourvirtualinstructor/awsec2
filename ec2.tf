b3085da01cb846932161712c708cc927bd93d021
Your Virtual Instructor <145182304+yourvirtualinstructor@users.noreply.github.com>
2023-09-18 17:36:28 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
 
