9f16d5028bd6e6ee7af1cdeaebf7d687f6067aa1
Your Virtual Instructor <145182304+yourvirtualinstructor@users.noreply.github.com>
2023-09-19 05:53:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.17.0"
+      version = "~> 5.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  region  = "us-east-2"
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
